83c2121034e3c0b890a9f58f2b008ee77255402d
wrinkl3 <al.seward@gmail.com>
2016-05-08 21:42:07 +0400

    Mockito test added

--- src/main/java/com/sdp/wrinkl3/VowelCounter.java
+++ src/main/java/com/sdp/wrinkl3/VowelCounter.java
@@ -1,12 +1,14 @@
 package com.sdp.wrinkl3;
 
 
+import java.util.List;
+
 public class VowelCounter {
 
     public static int countVowels(String s){
         int numVowels = 0;
         for(int i =0; i<s.length(); i++){
-            switch(s.charAt(i)){
+            switch(Character.toLowerCase(s.charAt(i))){
                 case 'a':
                 case 'e':
                 case 'i':
@@ -17,5 +19,12 @@ public class VowelCounter {
             }
         }
         return numVowels;
+    }
+
+    public static int countVowels(List<Character> l){
+        String s = new String();
+        for(int i = 0; i<l.size(); i++)
+            s = s.concat(Character.toString(l.get(i)));
+        return countVowels(s);
     }
 }
--- src/test/java/com/sdp/wrinkl3/VowelCounterTest.java
+++ src/test/java/com/sdp/wrinkl3/VowelCounterTest.java
@@ -1,16 +1,32 @@
 package com.sdp.wrinkl3;
 
 import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class VowelCounterTest {
+    @Mock List<Character> l;
 
     @Test
-    public void BasicTest(){
+    public void basicTest(){
         String testString = "geronimo";
         int num = VowelCounter.countVowels(testString);
         assertEquals(num, 4);
 
+    }
+
+    @Test
+    public void mockitoTest(){
+        l = mock(List.class);
+        when(l.get(0)).thenReturn('a');
+        when(l.get(1)).thenReturn('b');
+        when(l.get(2)).thenReturn('b');
+        when(l.get(3)).thenReturn('a');
+        when(l.size()).thenReturn(4);
+        assertEquals(VowelCounter.countVowels(l), 2);
     }
 }
